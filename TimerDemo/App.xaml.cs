ac8f027fb3bce1e1f7bf999b95ec12685709448a
oiliu <755422922@qq.com>
2020-11-10 15:28:17 +0800

    1、单进程启动

--- TimerDemo/App.xaml.cs
+++ TimerDemo/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,16 @@ namespace TimerDemo
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Process thisProc = Process.GetCurrentProcess();
+
+            if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+            {
+                Application.Current.Shutdown();
+                return;
+            }
+            base.OnStartup(e);
+        }
     }
 }
